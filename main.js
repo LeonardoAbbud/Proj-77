1b0ddbdcc7548cc6b7fcfce0ca85d2e632b59f2b
leonardobyjus <114768268+leonardobyjus@users.noreply.github.com>
2023-04-19 14:35:20 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+console.log("Meu primeiro projeto no console");
+
+            var firstName = "Leo";
+            var lastName = "Abbud";
+
+            var fullName = firstName.concat(lastName);
+            console.log(fullName);
+            // Nós usamos o método concat quando queremos 
+            //mesclar duas variáveis no console, 
+            //como por exemplo, 
+            //mesclar Leo e Abbud para que vire um nome só, 
+            //ou seja, para quem além de Leo e Abbud separados, 
+            //seja impresso também Leo Abbud
+
+            var a = 10;
+            var b = 5;
+
+            var add = a + b ;
+            console.log(add);
+
+            var sub = a - b ;
+            console.log(sub);
+
+            var multiple = a * b ;
+            console.log(multiple);
+
+            var divide = a / b ;
+            console.log(divide);
+
